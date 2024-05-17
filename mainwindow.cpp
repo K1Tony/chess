2dce6a519e46e7a67746a5d5e89b83673abccdea
Antoni Kucharski <antek.kuchar@gmail.com>
2024-05-17 12:22:25 +0200

    Chessboard squares colored

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,19 +3,26 @@
 
 #include <qlabel.h>
 
-void fill_chessboard(Chessboard &chessboard, Ui::MainWindow *ui);
-
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
     Chessboard chessboard;
+    int playing_area = this->width() * 0.8;
+    int square_length = playing_area / 8;
     for (int rank = 0; rank < 8; rank++) {
         for (int field = 0; field < 8; field++) {
             auto *label = new QLabel(ui->centralwidget);
+            label->setAutoFillBackground(true);
+            label->setMinimumSize(square_length, square_length);
+            label->setMaximumSize(square_length, square_length);
+            if ((rank + field) % 2 == 0) {
+                label->setStyleSheet("QLabel {background-color : #B56F07}");
+            } else {
+                label->setStyleSheet("QLabel {background-color : #FFEED4}");
+            }
             chessboard[rank][field].set_attributes(rank + 1, (Field) field, label);
-            label->setText(chessboard[rank][field].code().c_str());
             ui->gridLayout->addWidget(label, 7 - rank, field);
         }
     }
@@ -24,14 +31,4 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
-}
-
-void fill_chessboard(Chessboard &chessboard, Ui::MainWindow *ui) {
-    for (int rank = 0; rank < 8; rank++) {
-        for (int field = 0; field < 8; field++) {
-            auto *label = new QLabel();
-            ui->gridLayout->addWidget(label, 7 - rank, field);
-            chessboard[rank][field] = Square(rank + 1, (Field) field, label);
-        }
-    }
 }
