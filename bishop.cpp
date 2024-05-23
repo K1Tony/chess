045a2e6f27320e29ce65176f3e98f21c71846471
Antoni Kucharski <antek.kuchar@gmail.com>
2024-05-23 10:41:10 +0200

    Slight changes to legal moves - now members of Piece.

--- bishop.cpp
+++ bishop.cpp
@@ -8,18 +8,14 @@ Bishop::Bishop(Position position, PieceColor color) : Piece(position, color)
         pixmap_.reset(new QPixmap(":/graphics/resources/black-bishop.png"));
 }
 
-std::vector<Position> Bishop::legal_moves(
+void Bishop::available_moves(
     const std::unique_ptr< std::map<Position, std::shared_ptr<Piece> > > &white_positions,
     const std::unique_ptr< std::map<Position, std::shared_ptr<Piece> > > &black_positions)
 {
-    std::vector<Position> result;
-    result.reserve(13);
 
     if (color_ == WHITE) {
-        fill_diagonals(position_, white_positions, black_positions, result);
+        fill_diagonals(position_, white_positions, black_positions, legal_moves_);
     } else {
-        fill_diagonals(position_, black_positions, white_positions, result);
+        fill_diagonals(position_, black_positions, white_positions, legal_moves_);
     }
-
-    return result;
 }
